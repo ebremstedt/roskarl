033a1124127a05fe0d5375e4a03f08766aad9fe2
Erik Bremstedt <erik.bremstedt@gmail.com>
2024-11-23 06:59:46 +0100

    general function

--- roskarl/env.py
+++ roskarl/env.py
@@ -1,22 +1,39 @@
 import os
-from typing import List, Optional
+from typing import Optional
+from enum import Enum, auto
 
 
-def get_env_var(var: str, optional: bool = False) -> str:
+class EnvVarType(Enum):
+    STRING_VAR = auto()
+    LIST_VAR = auto()
+    BOOL_VAR = auto()
+
+
+def get_env_var(
+    var: str,
+    env_var_type: EnvVarType = EnvVarType.STRING_VAR,
+    optional: bool = False,
+    list_separator: str = ",",
+) -> str | list | bool:
     env_var = os.environ.get(var)
     if optional:
         return env_var
     if not env_var:
         raise KeyError(f"Missing required environment variable for '{var}'.")
 
-    return env_var
+    if env_var_type == EnvVarType.STRING_VAR:
+        return env_var
 
+    if env_var_type == EnvVarType.LIST_VAR:
+        try:
+            return [item.strip() for item in var.split(list_separator)]
+        except Exception as e:
+            raise ValueError(
+                f"Error parsing list from env var '{var}': {e}"
+            )
 
-def get_list_of_strings(input: Optional[str], separator: str) -> List[str]:
-    if input is None:
-        raise ValueError(f"The environment variable '{input}' is not set.")
-
-    try:
-        return [item.strip() for item in input.split(separator)]
-    except Exception as e:
-        raise ValueError(f"Error parsing the environment variable '{input}': {e}")
+    if env_var_type == EnvVarType.BOOL_VAR:
+        if not var:
+            return False
+        if var.upper() == "TRUE":
+            return True
