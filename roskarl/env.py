666758325a2727b12d5ee902a00c3a71781cc585
Erik Bremstedt <erik.bremstedt@gmail.com>
2024-11-23 07:32:34 +0100

    unit tests

--- roskarl/env.py
+++ roskarl/env.py
@@ -1,5 +1,4 @@
 import os
-from typing import Optional
 from enum import Enum, auto
 
 
@@ -7,17 +6,25 @@ class EnvVarType(Enum):
     STRING_VAR = auto()
     LIST_VAR = auto()
     BOOL_VAR = auto()
+    INT_VAR = auto()
 
 
 def get_env_var(
     var: str,
     env_var_type: EnvVarType = EnvVarType.STRING_VAR,
-    optional: bool = False,
     list_separator: str = ",",
-) -> str | list | bool:
+) -> str | list | bool | int:
+    """Get environment variable
+
+    Parameters:
+        var: the var to get
+        env_var_type (EnvVarType): the kind of value you expect to retrieve from var
+        list_separator: if getting list, which separator to use
+
+    Returns:
+        value of env var
+    """
     env_var = os.environ.get(var)
-    if optional:
-        return env_var
     if not env_var:
         raise KeyError(f"Missing required environment variable for '{var}'.")
 
@@ -28,12 +35,18 @@ def get_env_var(
         try:
             return [item.strip() for item in var.split(list_separator)]
         except Exception as e:
-            raise ValueError(
-                f"Error parsing list from env var '{var}': {e}"
-            )
+            raise ValueError(f"Error parsing list from env var '{var}': {e}")
 
     if env_var_type == EnvVarType.BOOL_VAR:
-        if not var:
-            return False
-        if var.upper() == "TRUE":
+        if env_var.upper() == "TRUE":
             return True
+        if env_var.upper() == "False":
+            return False
+
+        raise ValueError(f"Bool must be set to true or false '{env_var}': {e}")
+
+    if env_var_type == EnvVarType.INT_VAR:
+        if env_var.isnumeric():
+            return int(env_var)
+
+        raise ValueError(f"Int must be set to a valid integer '{env_var}': {e}")
--- tests/test_env.py
+++ tests/test_env.py
@@ -1,2 +1,80 @@
-def test_add():
-    assert 3 == 3  # This will pass
+import os
+from roskarl.env import get_env_var, EnvVarType
+from unittest import mock, TestCase
+
+# def test_get_string_var():
+#     env_var = "MY_VAR=Hello World"
+#     with mock.patch.dict(os.environ, {"MY_VAR": env_var}):
+#         assert get_env_var("MY_VAR") == "Hello World"
+
+# def test_get_list_var():
+#     env_var = "LIST_VAR=A,B,C,D"
+#     with mock.patch.dict(os.environ, {"LIST_VAR": env_var}):
+#         assert get_env_var("LIST_VAR", env_var_type=EnvVarType.LIST_VAR) == ["A", "B", "C", "D"]
+
+# def test_get_list_var_with_separator():
+#     env_var = "LIST_VAR=A|B|C|D"
+#     with mock.patch.dict(os.environ, {"LIST_VAR": env_var}):
+#         assert get_env_var("LIST_VAR", list_separator="|") == ["A", "B","C", "D"]
+
+# def test_get_list_var_with_invalid_separator():
+#     env_var = "LIST_VAR=A,B,C,D"
+#     with mock.patch.dict(os.environ, {"LIST_VAR": env_var}):
+#         try:
+#             get_env_var("LIST_VAR", list_separator="|")
+#             assert False
+#         except ValueError as e:
+#             assert str(e) == "Error parsing list from env var 'LIST_VAR': can't split string by '|'"
+
+# def test_get_bool_var():
+#     env_var = "BOOL_VAR=true"
+#     with mock.patch.dict(os.environ, {"BOOL_VAR": env_var}):
+#         assert get_env_var("BOOL_VAR", env_var_type=EnvVarType.BOOL_VAR) is True
+
+# def test_get_bool_var_invalid_value():
+#     env_var = "BOOL_VAR=bogus"
+#     with mock.patch.dict(os.environ, {"BOOL_VAR": env_var}):
+#         try:
+#             get_env_var("BOOL_VAR", env_var_type=EnvVarType.BOOL_VAR)
+#             assert False
+#         except ValueError as e:
+#             assert str(e) == "Bool must be set to true or false 'bogus'"
+
+# def test_get_int_var():
+#     env_var = "INT_VAR=123"
+#     with mock.patch.dict(os.environ, {"INT_VAR": env_var}):
+#         assert get_env_var("INT_VAR", env_var_type=EnvVarType.INT_VAR) == 123
+
+# def test_get_int_var_invalid_value():
+#     env_var = "INT_VAR=bogus"
+#     with mock.patch.dict(os.environ, {"INT_VAR": env_var}):
+#         try:
+#             get_env_var("INT_VAR", env_var_type=EnvVarType.INT_VAR)
+#             assert False
+#         except ValueError as e:
+#             assert str(e) == "Int must be set to a valid integer 'bogus'"
+
+# def test_get_env_var_missing_value():
+#     with mock.patch.dict(os.environ, {}):
+#         try:
+#             get_env_var("NON_EXISTENT_VAR")
+#             assert False
+#         except KeyError as e:
+#             assert str(e) == "Missing required environment variable for 'NON_EXISTENT_VAR.'"
+
+class TestGetEnvVar(TestCase):
+    def test_get_env_var_string(self):
+        env_var_value = "hello"
+        env_var_name = "x"
+        os.environ[env_var_name] = env_var_value
+
+        real_value = get_env_var(var=env_var_name)
+
+        assert env_var_value == real_value
+
+
+    def test_get_env_var_empty(self):
+        env_var_name = "this_var_isnt_set_at_all"
+
+        with self.assertRaises(KeyError):
+            get_env_var(var=env_var_name)
